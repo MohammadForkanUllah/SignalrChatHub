1eb560c2369a4434ecf0480cdbb998aa960ee6cf
IDB _C# <IDB_C#@nh.ctg>
2023-10-11 13:06:19 +0600

    first commit

---
+++ Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(EvidenceSignalR.Startup))]
+
+namespace EvidenceSignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
